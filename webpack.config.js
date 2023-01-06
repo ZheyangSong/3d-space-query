28a78a926d99ad07234e1318eb72470606f4a74d
Zheyang Song <jerome_soung@hotmail.com>
2023-01-06 02:59:50 -0800

    Initial Commit
    - set up basic tool chain

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+// Generated using webpack-cli https://github.com/webpack/webpack-cli
+
+const path = require("path");
+
+const isProduction = process.env.NODE_ENV == "production";
+
+const config = {
+  devtool: "source-map",
+  entry: "./src/index.ts",
+  output: {
+    filename: "index.js",
+    path: path.resolve(__dirname, "dist/umd"),
+    globalObject: "globalThis",
+    library: {
+      name: "GeometryQueryBuilder",
+      type: "umd",
+    },
+  },
+  plugins: [
+    // Add your plugins here
+    // Learn more about plugins from https://webpack.js.org/configuration/plugins/
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.ts$/i,
+        loader: "ts-loader",
+        exclude: ["/node_modules/"],
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".ts", ".js", "..."],
+  },
+};
+
+module.exports = () => {
+  if (isProduction) {
+    config.mode = "production";
+  } else {
+    config.mode = "development";
+  }
+  return config;
+};
